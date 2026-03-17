be7537a1335fa74142e11847a3943946ea11cf74
Ricardo Cerqueira <ricardo.cerqueira@outlook.com>
2026-03-17 06:36:23 -0400

    MSGraph-Module.ps1
    This PowerShell command installs the Microsoft Graph PowerShell SDK module

---
+++ PowerShell/EntraID/Commands/MSGraph-Module.ps1
@@ -0,0 +1,6 @@
+# To use Microsoft Graph PowerShell, you need:
+# • A supported operating system (Windows 10 or later, or supported Windows Server).
+# • PowerShell 5.1 or newer, or PowerShell Core. PowerShell 7 or later is recommended
+# • The Microsoft Graph PowerShell SDK
+
+Install-Module -Name Microsoft.Graph -Scope CurrentUser
